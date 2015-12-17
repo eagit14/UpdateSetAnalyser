9f4efdf3ea3f8500a9be01fd028034b703970d7e
eagit14 <elie.abdelnour@gmail.com>
2015-12-17 14:48:55 +0100

    Main class enhancement on log

--- USAnalyser/src/ea/sn/usa/Main.java
+++ USAnalyser/src/ea/sn/usa/Main.java
@@ -15,7 +15,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.print("Hello World");
+        System.out.print("Hello World - enhancement");
     }
     
 }
