99b728a490f0b7de7e77f3680e5e70e954224434
eagit14 <elie.abdelnour@gmail.com>
2015-12-17 18:17:05 +0100

    SOAP Web service

--- USAnalyser/src/ea/sn/usa/Main.java
+++ USAnalyser/src/ea/sn/usa/Main.java
@@ -17,7 +17,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.print("Hello World - enhancement");
+        System.out.print("Starting Webnow");
         EFrame myFrame = new EFrame();
         myFrame.buildAndShowFrame();
     }
